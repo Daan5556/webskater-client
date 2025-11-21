f3f4e9ad00bb5f68de2893f04f11d67670b7f2c7
Daan <egdaan@gmail.com>
2025-11-21 15:33:55 +0100

    Cleaned up code

--- src/browser.py
+++ src/browser.py
@@ -94,10 +94,53 @@ class URL:
         return content
 
 
+@dataclass
+class Text:
+    text: str
+
+
+@dataclass
+class Tag:
+    tag: str
+
+
+def lex(body):
+    out = []
+    buffer = ""
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+            if buffer:
+                out.append(Text(buffer))
+            buffer = ""
+        elif c == ">":
+            in_tag = False
+            out.append(Tag(buffer))
+            buffer = ""
+        else:
+            buffer += c
+    if not in_tag and buffer:
+        out.append(Text(buffer))
+
+    return out
+
+
+FONTS = {}
+
+
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight, slant=style)
+        label = tkinter.Label(font=font)
+        FONTS[key] = (font, label)
+    return FONTS[key][0]
+
+
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
 SCROLL_STEP = 100
-FONTS = {}
 
 
 class Layout:
@@ -244,47 +287,6 @@ class Browser:
         if self.scroll > self.max_scroll:
             self.scroll = self.max_scroll
         self.draw()
-
-
-@dataclass
-class Text:
-    text: str
-
-
-@dataclass
-class Tag:
-    tag: str
-
-
-def lex(body):
-    out = []
-    buffer = ""
-    in_tag = False
-    for c in body:
-        if c == "<":
-            in_tag = True
-            if buffer:
-                out.append(Text(buffer))
-            buffer = ""
-        elif c == ">":
-            in_tag = False
-            out.append(Tag(buffer))
-            buffer = ""
-        else:
-            buffer += c
-    if not in_tag and buffer:
-        out.append(Text(buffer))
-
-    return out
-
-
-def get_font(size, weight, style):
-    key = (size, weight, style)
-    if key not in FONTS:
-        font = tkinter.font.Font(size=size, weight=weight, slant=style)
-        label = tkinter.Label(font=font)
-        FONTS[key] = (font, label)
-    return FONTS[key][0]
 
 
 if __name__ == "__main__":
