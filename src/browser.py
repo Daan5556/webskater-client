b32f719433f637c6e9a5273ff96aa68a75f3bcca
Daan Eggen <egdaan@gmail.com>
2026-01-14 11:12:26 +0100

    Added parser

--- src/browser.py
+++ src/browser.py
@@ -1,9 +1,10 @@
+from __future__ import annotations
 import socket
 import ssl
 import tkinter
 import tkinter.font
-from typing import Literal
-from dataclasses import dataclass
+from typing import Dict, Literal
+from dataclasses import dataclass, field
 
 from src.emoji import get_emoji_data, is_emoji
 
@@ -95,35 +96,24 @@ class URL:
 
 
 @dataclass
-class Text:
-    text: str
+class Element:
+    tag: str
+    attributes: Dict
+    parent: Element | None
+    children: list["Element"] = field(default_factory=list)
+
+    def __repr__(self) -> str:
+        return "<" + self.tag + ">"
 
 
 @dataclass
-class Tag:
-    tag: str
+class Text:
+    text: str
+    parent: Element
+    children: list["Element"] = field(default_factory=list)
 
-
-def lex(body):
-    out = []
-    buffer = ""
-    in_tag = False
-    for c in body:
-        if c == "<":
-            in_tag = True
-            if buffer:
-                out.append(Text(buffer))
-            buffer = ""
-        elif c == ">":
-            in_tag = False
-            out.append(Tag(buffer))
-            buffer = ""
-        else:
-            buffer += c
-    if not in_tag and buffer:
-        out.append(Text(buffer))
-
-    return out
+    def __repr__(self) -> str:
+        return repr(self.text)
 
 
 FONTS = {}
@@ -206,6 +196,97 @@ class Layout:
 
         self.cursor_x = HSTEP
         self.line = []
+
+
+class HTMLParser:
+    def __init__(self, body):
+        self.body = body
+        self.unfinished = []
+        self.SELF_CLOSING_TAGS = [
+            "area",
+            "base",
+            "br",
+            "col",
+            "embed",
+            "hr",
+            "img",
+            "input",
+            "link",
+            "meta",
+            "param",
+            "source",
+            "track",
+            "wbr",
+        ]
+
+    def add_text(self, text):
+        if text.isspace():
+            return
+        parent = self.unfinished[-1]
+        node = Text(text, parent)
+        parent.children.append(node)
+
+    def add_tag(self, tag):
+        tag, attributes = self.get_attributes(tag)
+        if tag.startswith("!"):
+            return
+        if tag.startswith("/"):
+            if len(self.unfinished) == 1:
+                return
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        elif tag in self.SELF_CLOSING_TAGS:
+            parent = self.unfinished[-1]
+            node = Element(tag, attributes, parent)
+            parent.children.append(node)
+
+        else:
+            parent = self.unfinished[-1] if self.unfinished else None
+            node = Element(tag, attributes, parent)
+            self.unfinished.append(node)
+
+    def get_attributes(self, text):
+        parts = text.split()
+        tag = parts[0].casefold()
+        attributes = {}
+        for attrpair in parts[1:]:
+            if "=" in attrpair:
+                key, value = attrpair.split("=", 1)
+                if len(value) > 2 and value[0] in ["'", '"']:
+                    value = value[1:-1]
+                attributes[key.casefold()] = value
+            else:
+                attributes[attrpair.casefold()] = ""
+
+        return tag, attributes
+
+    def finish(self):
+        while len(self.unfinished) > 1:
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        return self.unfinished.pop()
+
+    def parse(self):
+        text = ""
+        in_tag = False
+        for c in self.body:
+            if c == "<":
+                in_tag = True
+                if text:
+                    self.add_text(text)
+                text = ""
+            elif c == ">":
+                in_tag = False
+                self.add_tag(text)
+                text = ""
+            else:
+                text += c
+        if not in_tag and text:
+            self.add_text(text)
+
+        return self.finish()
 
 
 class Browser:
